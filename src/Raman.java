41a04c46f6613b5401d81ec4dedeeb7cf058ea5d
MayankTiwari9 <mt1027319@gmail.com>
2024-01-12 12:41:32 +0530

    DSA Test

---
+++ src/Raman.java
@@ -0,0 +1,35 @@
+public class Raman {
+    public static int raman(int n, int k, String s){
+        int score = 0;
+        int zeros = 0;
+        for (char c: s.toCharArray()) {
+            if(c == '0'){
+                zeros++;
+                if(zeros == k){
+                    score++;
+                    zeros = 0;
+                }
+            }
+            else{
+                zeros = 0;
+            }
+        }
+        return score;
+    }
+    public static void main(String[] args) {
+        // int n = 4;
+        // int k = 1;
+        // String s = "1010";
+
+        // int n = 4;
+        // int k = 2;
+        // String s = "0100";
+
+        
+        int n = 11;
+        int k = 3;
+        String s = "00100000001";
+
+        System.out.println(raman(n,k,s));
+    }
+}
---
+++ src/StringBeauty.java
@@ -0,0 +1,21 @@
+public class StringBeauty {
+    static int stringBeauty(String k1, String k2){
+        String lowerCaseK1 = k1.toLowerCase();
+        String lowerCaseK2 = k2.toLowerCase();
+
+        int diff = 0;
+
+        for (int i = 0; i < k1.length(); i++) {
+            if(lowerCaseK1.charAt(i) != lowerCaseK2.charAt(i))
+            diff++;
+        }
+
+        return diff;
+    }
+    public static void main(String[] args) throws Exception {
+        String k1 = "abcde";
+        String k2 = "AbCdE";
+
+        System.out.println(stringBeauty(k1, k2));
+    }
+}
